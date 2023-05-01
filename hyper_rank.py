c51b5d2d014b83b183521146f77c67f5d733abc5
Tom <tdevito@icloud.com>
2023-05-01 12:54:47 -0400

    Spring updates

--- hyper_rank.py
+++ hyper_rank.py
@@ -1,80 +1,82 @@
-from dataset import user as cu
-from dataset import users as u
-from dataset import items as i
+from typing import List, Dict
 
 class HyperRank():
+    # Define the HyperRank class, which will contain the functions necessary for ranking the items
 
-    def hyper_rank(self, current_user, user_data, item_data):
+    def assign_item_integers(self, item_data: List[Dict[str, any]]) -> List[Dict[str, any]]:
 
-        tier_A = set()
-        tier_B = set()
-        tier_C = set()
+        # This function takes in a list of dictionaries, each containing item data and a list of votes.
+        # It assigns each item a numerical integer based on the number of votes it has.
+        # The updated item data with the integer value is returned as a list of dictionaries.
 
-        #loop through item_data and assign a temporary integer to item_data based on raw vote
+        updated_item_data = []
+
         for item in item_data:
             votes = item["votes"]
-            print("item: ", item)
             item_int = len(votes)
-            print(item_int)
-            item["item_int"] = item_int
-            print("item: ", item)
+            new_item = dict(item)
+            new_item["item_int"] = item_int
+            updated_item_data.append(new_item)
 
-        #rank items by number of votes in descending order
-        print(item_data)
-        item_data = sorted(item_data)
-        print(item_data)
+        return updated_item_data
 
-        #tier items
-        item_data_quarter = len(item_data) / 4
-        item_count = 0
 
-        for item in item_data:
-            #push the top 25% of items into tier_A
-            if item_count < item_data_quarter:
-                tier_A.add(item)
-                item_count = item_count + 1
-            #push the bottom 25% of items into tier_C
-            elif item_count > item_data_quarter * 3:
-                tier_C.add(item)
-                item_count = item_count + 1
-            #push the middle 50% of items into tier_B
-            else:
-                tier_B.add(item)
-                item_count = item_count + 1
+    def rank_items(self, item_data: List[Dict[str, any]]) -> List[Dict[str, any]]:
 
-        #identify the intersection of the current_user vote array to users_data vote array
-        current_user_votes = current_user["votes"]
-        intersection_users = set()
-        for user in user_data:
-            user_votes = user["votes"]
-            vote_intersection = current_user_votes.intersection(user_votes)
-            #assign a temporary integer to each user where an intersection exists
-            if len(vote_intersection) > 0:
-                temp_int = len(vote_intersection)
-                uid = user["uid"]
-                intersection_user = {}
-                intersection_user["temp_int"] = temp_int
-                intersection_user["uid"] = uid
-                intersection_users.add(intersection_user)
-            
-        #loop through item_data votes array to check if any intersection uids exist
-        item_intersections = []
-        for item in item_data:
-            item_vote = item["votes"]
-            for uids in intersection_users:
-                user_uid = uids["uid"]
-                user_int = uids["temp_int"]
-                if user_uid == item_vote:
-                    item_vote_length = len(item_vote)
-                    item_vote_length = item_vote_length + user_int
-           
-        #add the user integer - 1 to the item_data integer
+        # This function takes in a list of dictionaries containing item data and their assigned integers.
+        # It ranks the items into three tiers: A, B, and C based on their assigned integer values.
+        # It then modifies the integer values of the items in tier B and tier C.
+        # Finally, it returns the sorted list of items based on their modified integer values.
 
-        #loop through items in tier_b and square the item integer
-        #loop through items in tier_c and cube the item integer
+        sorted_items = sorted(item_data, key=lambda x: x["item_int"], reverse=True)
+        item_data_quarter = len(item_data) // 4
+        tier_A = sorted_items[:item_data_quarter]
+        tier_B = sorted_items[item_data_quarter:(3 * item_data_quarter)]
+        tier_C = sorted_items[(3 * item_data_quarter):]
 
-        #rank item_data items by updated integer in descending order
-        #return item_data as dictionary
+        for item in tier_B:
+            item["item_int"] = item["item_int"] ** 2
 
+        for item in tier_C:
+            item["item_int"] = item["item_int"] ** 3
+
+        sorted_items = sorted(item_data, key=lambda x: x["item_int"], reverse=True)
+        return sorted_items
+
+
+    def hyper_rank(self, current_user: Dict[str, any], user_data: List[Dict[str, any]], item_data: List[Dict[str, any]]) -> List[Dict[str, any]]:
+
+        # This function takes in a dictionary of votes for the current user, a list of dictionaries containing user data,
+        # and a list of dictionaries containing item data and their assigned integers.
+        # It assigns each item an integer value, ranks them, and returns the sorted list of items.
+
+        item_data = self.assign_item_integers(item_data)
+        ranked_items = self.rank_items(item_data)
+
+        return ranked_items
+
+
+# example usage
 hr = HyperRank()
-print(hr.hyper_rank(cu, u, i))
+
+# Create an example current user dictionary and example lists of user and item dictionaries
+current_user = {"votes": ["item1", "item2"]}
+
+user_data = [    
+    {"uid": "user1", "votes": ["item1", "item2", "item4"]},
+    {"uid": "user2", "votes": ["item2", "item3", "item5"]},
+    {"uid": "user3", "votes": ["item3", "item4", "item6"]},
+    {"uid": "user4", "votes": ["item1", "item3", "item5", "item6"]},
+    {"uid": "user5", "votes": ["item2", "item4", "item6"]}
+]
+
+item_data = [    
+    {"item_id": "item1", "votes": ["user1", "user4"]},
+    {"item_id": "item2", "votes": ["user1", "user2", "user5"]},
+    {"item_id": "item3", "votes": ["user2", "user3", "user4"]},
+    {"item_id": "item4", "votes": ["user1", "user3", "user5"]},
+    {"item_id": "item5", "votes": ["user2", "user4"]},
+    {"item_id": "item6", "votes": ["user3", "user4", "user5"]}
+]
+ranked_items = hr.hyper_rank(current_user, user_data, item_data)
+print(ranked_items)
--- readme.md
+++ readme.md
@@ -1,40 +1,19 @@
 ## Project Overview
 
-Hyper Rank is a user-user collaborative filtering algorithm that I have implemented here in Python. It is designed to 
-be extremely robust even with small inputs so it is an excellent choice for applications with a small dataset. This is still
-a work in progress and I'm expolring an implementaion using TensorFlow. 
+Hyper Rank is a user-user collaborative filtering algorithm that I have implemented here in Python.
 
 ## Hyper Rank Function
 
-This is the mathematical notaion:
+Mathematically can be written as:
 
 ![](http://www.sciweavers.org/tex2img.php?eq=hr%28%20%5Cchi%20%29%20%3D%20log%5Csum_%7BU%20%5Ckappa%20%20%5Cepsilon%20U%7D%5E%7BUn%7D%20%28%28%7BCU%7D%20%7B%5Cbigcap_%7D%20%7BU%20%5Ckappa%20%7D%29%20%5CRe%20%29Un&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0)
 
-where
- 
-![](http://www.sciweavers.org/tex2img.php?eq=%20%5Cchi%20%20%5Cepsilon%20%5Cbig%5C%7BTA%5Cbig%5C%20%7B%5Cbigvee_%7D%20%7BTB%5E%7B2%7D%7D%20%5Cbig%5C%20%5Cbigvee_%7D%20TC%5E%7B3%7D%5Cbig%5C%7D%20&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0)
-
-
-The hyper_rank function in Python HR(CU, U, I) -> I' can be defined as follows:
-
-1. Loop through item_data(I) and assign a temporary integer(item_int) to item_data based on vote count
-2. Rank items by item_int in descending order
-3. Push the top 25% of items into tier_A
-4. Push the middle 50% of items into tier_B
-5. Push the bottom 25% of items into tier_C
-6. Identify the intersection of the current_user(CU) vote array to user_data(U) vote arrays
-7. Assign a temporary integer(user_user_int) to each user where an intersection exists
-8. Loop through item_data votes array to check if any intersection(user_item_int) with user_user_int exists
-9. If intersection exists, loop through user_item_int
-10. Subtract the number of user_item_int from the item_int
-11. Multiply the items user_item_int by the user_user_int
-12. Add the result of step 10 to the result of step 11
-13. Log the result of step 12
-14. Loop through items in tier_b and square the item integer
-15. Loop through items in tier_c and cube the item integer
-16. Rank item_data items by updated integer in descending order
-17. Return item_data(I') as dictionary
-
-## Set Partition
-
-Item x is a member of set {T}, which is partitioned into three subsets {TA, TB, TC} based on the items popularity within the entire set. The vote count of items in {TA} are unchanged, items in {TB} are squared, {TC} items are cubed. {TA} respresents items in the top 25% of most popular items, {TB}, between 25% and 75% and {TC}, the bottom 25%. The usefulness of partitioning {T} into three subsets is to give further weight to items that two users have a common interest in but is not generally popular in the set as a whole. This metric adds a much higher degree of similarity between two users when the function is executed.
+Given a set of users and items, represented as user_data and item_data respectively, we can calculate a numerical ranking score for each item using the HyperRank algorithm.
+The HyperRank algorithm involves the following steps:
+Assign an integer value to each item based on the number of votes it has received from users.
+Rank the items based on their integer values, with higher values indicating a higher rank.
+Divide the ranked items into three tiers: A, B, and C.
+Apply a scaling factor to the integer values of items in tiers B and C, with the scaling factor increasing for each tier.
+Re-rank the items based on their updated integer values.
+Once the items have been ranked, we can recommend them to a current user based on their past votes.
+The HyperRank class provides methods to perform each of these steps, and the hyper_rank method combines them to generate a ranked list of items that the current user may be interested in.
